7189309268958fb24372b2b91f8f36707dd3b6c9
svitlanapl <sveta_pr@i.ua>
2022-10-09 23:55:13 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    bodyEl: document.querySelector('body'),
+    btnStartEl: document.querySelector('[data-start]'),
+    btnStopEl: document.querySelector('[data-stop]'),
+}
+let idInterval = null;
+refs.btnStopEl.disabled = true;
+ 
+refs.btnStartEl.addEventListener('click', onbtnStart);
+refs.btnStopEl.addEventListener('click', onbtnStop);
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onbtnStart() {
+    idInterval = setInterval(() => {
+        refs.bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.btnStartEl.disabled = true;
+    refs.btnStopEl.disabled = false;
+}
+
+function onbtnStop() {
+    clearInterval(idInterval);
+    refs.btnStartEl.disabled = false;
+    refs.btnStopEl.disabled = true;
+}
