f66e1602768a6d78c699afa6ad817dc38257e080
svitlanapl <105806771+svitlanapl@users.noreply.github.com>
2022-10-09 20:55:59 +0000

    Deploying to gh-pages from @ svitlanapl/goit-js-hw-09@7189309268958fb24372b2b91f8f36707dd3b6c9 🚀

---
+++ 01-color-switcher.15552803.js
@@ -0,0 +1,2 @@
+const t={bodyEl:document.querySelector("body"),btnStartEl:document.querySelector("[data-start]"),btnStopEl:document.querySelector("[data-stop]")};let e=null;t.btnStopEl.disabled=!0,t.btnStartEl.addEventListener("click",(function(){e=setInterval((()=>{t.bodyEl.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.btnStartEl.disabled=!0,t.btnStopEl.disabled=!1})),t.btnStopEl.addEventListener("click",(function(){clearInterval(e),t.btnStartEl.disabled=!1,t.btnStopEl.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.15552803.js.map
